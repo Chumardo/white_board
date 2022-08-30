c563eaff280bd032199532b062b4ee1c30c2cf82
chumardo <dachigiorgadze98@gmail.com>
2022-08-30 20:14:22 +0400

    tools with icons

--- white_board.py
+++ white_board.py
@@ -13,7 +13,7 @@ color_frame = LabelFrame(window, text='Colors', relief=RIDGE, bg="white", font=(
 color_frame.place(x=10,y=10, width=665, height=75)
 
 tool_frame = LabelFrame(window, text='Tools', relief=RIDGE, bg="white", font=('arial', 15, 'bold'))
-tool_frame.place(x=800,y=10, width=200, height=75)
+tool_frame.place(x=800,y=10, width=190, height=75)
 
 pen_size = LabelFrame(window, text='Pen Size', relief=RIDGE, bg="white", font=('arial', 15, 'bold'))
 pen_size.place(x=1100,y=10, width=220, height=75)
@@ -27,6 +27,23 @@ i=j=0
 for color in colors:
     Button(color_frame, bd=3, bg=color, relief=RIDGE, width=3).grid(row=j, column=i, padx=1)
     i += 1
+
+canvas_button = PhotoImage(file='images/canvas.png', height=30, width=30)
+canvas_color_b1 = Button(tool_frame, image=canvas_button,text="Canvas")
+canvas_color_b1.grid(row=0,column=0, padx=5)
+
+save_button = PhotoImage(file='images/save.png', height=30, width=30)
+save_b2 = Button(tool_frame, image=save_button, text="Save")
+save_b2.grid(row=0, column=1, padx=5)
+
+
+eraser_button = PhotoImage(file='images/eraser.png', height=30, width=30)
+eraser_b3 = Button(tool_frame, image=eraser_button, text="Eraser")
+eraser_b3.grid(row=0, column=3, padx=5)
+
+clear_button = PhotoImage(file='images/clear.png', height=30, width=30)
+clear_b4 = Button(tool_frame, image=clear_button, text="Clear")
+clear_b4.grid(row=0, column=4, padx=5)
 
 
 window.mainloop()
